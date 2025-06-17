6e6a10e8c50078ac2c800f989046e65dc35b427e
Jorge <jorge.martinezcruz@gmail.com>
2025-06-17 00:49:10 -0300

    nuevas mesas de trabajo

---
+++ src/modelo/MesaDeFlechas.java
@@ -0,0 +1,10 @@
+package modelo;
+
+import java.util.List;
+
+public class MesaDeFlechas extends MesaDeTrabajo {
+
+	public MesaDeFlechas(List<Receta> recetasDesbloqueadas) {
+		super("Mesa de Flechas", recetasDesbloqueadas);
+	}
+}
---
+++ src/modelo/MesaDePiedra.java
@@ -0,0 +1,10 @@
+package modelo;
+
+import java.util.List;
+
+public class MesaDePiedra extends MesaDeTrabajo {
+
+	public MesaDePiedra(List<Receta> recetasDesbloqueadas) {
+		super("Mesa De Piedra", recetasDesbloqueadas);
+	}
+}
