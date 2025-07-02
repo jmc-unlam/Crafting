f28dc950b1c9987677aa14b02feda14a7614fe87
EmilianoAlor <emigrow@yahoo.com.ar>
2025-07-02 17:10:34 -0300

    Vuelta a subir INI con mesa incluida.
    Por algun tema q desconosco, quite los archivos de ini... seguramente
    por algun conflicto presione algo q no debia....

--- src/prolog/inventario.pl
+++ src/prolog/inventario.pl
@@ -1,10 +1,11 @@
 % Objetos en el inventario
-inventario('mineral de plata',3).
-inventario('lingote de hierro',2).
-inventario('Mesa de Generica',1).
 inventario('Mesa de Flechas',1).
-inventario('carbon',8).
-inventario('mineral de oro',3).
-inventario('mineral de estano',4).
-inventario('Punta de Flecha',20).
-inventario('mineral de cobre',3).
+inventario('Téndon',2).
+inventario('Carcaj',1).
+inventario('Arco compuesto',1).
+inventario('Flechas',15).
+inventario('Madera',50).
+inventario('mineral de hierro',50).
+inventario('Hierro',5).
+inventario('Cuerda',49).
+inventario('ladrillo',50).
--- src/prolog/recetas.pl
+++ src/prolog/recetas.pl
@@ -1,28 +1,36 @@
 % Recetas
 % Lista de Objetos Crafteables 
-ingredientecompuesto('lingote de acero').
-ingredientecompuesto('Mesa de Generica').
 ingredientecompuesto('lingote de hierro').
-ingredientecompuesto('lingote de estano').
 ingredientecompuesto('Mesa de Flechas').
-ingredientecompuesto('lingote de bronze').
-ingredientecompuesto('lingote de plata').
-ingredientecompuesto('lingote de oro').
-ingredientecompuesto('lingote de cobre').
+ingredientecompuesto('Palo').
+ingredientecompuesto('Carcaj').
+ingredientecompuesto('Hacha de Piedra').
+ingredientecompuesto('Arco compuesto').
+ingredientecompuesto('Flechas').
+ingredientecompuesto('Oxido').
+ingredientecompuesto('Antorcha').
 ingredientecompuesto('Punta de Flecha').
+ingredientecompuesto('Equipamiento para Arquero').
 
 % Ingredientes por objeto
-ingrediente('lingote de acero', 'lingote de hierro', 2).
-ingrediente('lingote de acero', 'carbon', 8).
-ingrediente('Mesa de Generica', 'madera', 10).
-ingrediente('Mesa de Generica', 'ladrillo', 20).
 ingrediente('lingote de hierro', 'mineral de hierro', 7).
-ingrediente('lingote de estano', 'mineral de estano', 4).
-ingrediente('Mesa de Flechas', 'madera', 10).
+ingrediente('Mesa de Flechas', 'Madera', 10).
 ingrediente('Mesa de Flechas', 'ladrillo', 20).
-ingrediente('lingote de bronze', 'lingote de estano', 1).
-ingrediente('lingote de bronze', 'lingote de cobre', 4).
-ingrediente('lingote de plata', 'mineral de plata', 3).
-ingrediente('lingote de oro', 'mineral de oro', 3).
-ingrediente('lingote de cobre', 'mineral de cobre', 3).
+ingrediente('Palo', 'Madera', 1).
+ingrediente('Carcaj', 'Madera', 2).
+ingrediente('Carcaj', 'Cuerda', 3).
+ingrediente('Hacha de Piedra', 'Madera', 2).
+ingrediente('Hacha de Piedra', 'Piedra', 3).
+ingrediente('Hacha de Piedra', 'Oxido', 2).
+ingrediente('Arco compuesto', 'Téndon', 2).
+ingrediente('Arco compuesto', 'Madera', 1).
+ingrediente('Arco compuesto', 'Cuerda', 1).
+ingrediente('Flechas', 'Madera', 1).
+ingrediente('Flechas', 'Punta de Flecha', 3).
+ingrediente('Oxido', 'Hierro', 7).
+ingrediente('Antorcha', 'Carbon', 1).
+ingrediente('Antorcha', 'Palo', 1).
 ingrediente('Punta de Flecha', 'lingote de hierro', 1).
+ingrediente('Equipamiento para Arquero', 'Flechas', 15).
+ingrediente('Equipamiento para Arquero', 'Arco compuesto', 1).
+ingrediente('Equipamiento para Arquero', 'Carcaj', 1).
